f17eaf635db702a06018c713256c2c0549a261c4
freddie784 <83510059+freddie784@users.noreply.github.com>
2021-09-16 21:45:59 -0400

    created a d3 event funcction that looked for a button to be clicked so it could return filtered data

--- js/app.js
+++ js/app.js
@@ -20,3 +20,20 @@ function buildTable (data) {
     } );
 }
 
+function handleClick () {
+    
+    let date = d3.select("#datetime").property("value"); //Grab datetime value from filter
+    let filteredData = tableData; 
+
+    // Check to see if data entered and filter if so
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+        buildTable(filteredData); //run function using filtered data if no date used original table will be used
+    };
+}
+
+//attach an event of button being clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//build unfiltered table
+buildTable(tableData);
